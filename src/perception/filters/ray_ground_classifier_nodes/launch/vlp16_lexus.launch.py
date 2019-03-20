0750a52689fb482146a76d76fe535e4f6e3a9262
Esteve Fernandez <esteve@apache.org>
2019-03-20 10:59:44 +0100

    First cut at the ray ground filter nodes port

---
+++ src/perception/filters/ray_ground_classifier_nodes/launch/vlp16_lexus.launch.py
@@ -0,0 +1,26 @@
+"""Launch a few nodes for easy debugging and testing of the kinematic tracker"""
+
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+  driver = launch_ros.actions.Node(
+      package='velodyne_node', node_executable='velodyne_block_node_exe',
+      arguments=['--node_name=vlp16_front'])
+  classifier = launch_ros.actions.Node(
+      package='ray_ground_classifier_nodes', node_executable='ray_ground_classifier_block_node_exe',
+      arguments=['--node_name=ray_ground_classifier'])
+  viz_front = launch_ros.actions.Node(
+      package='autoware_rviz', node_executable='autoware_rviz',
+      arguments=['--block_topic=points_ground', '--block_viz_topic=ground_block_viz'])
+  viz_rear = launch_ros.actions.Node(
+      package='autoware_rviz', node_executable='autoware_rviz',
+      arguments=['--block_topic=points_nonground', '--block_viz_topic=nonground_block_viz'])
+  console = launch_ros.actions.Node(
+      package='apex_console_viewer',
+      node_executable='apex_console_viewer',
+      arguments=['--cm'],
+      output='screen')
+  return launch.LaunchDescription([driver, classifier, viz_front, viz_rear, console])
+
