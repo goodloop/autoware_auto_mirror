56f4108153d890de55fe2dfb8198e0840ce481ab
Florian Walbroel <walbroel@silexica.com>
2019-07-03 20:27:42 +0200

    added iterative quick sort implementation

---
+++ src/common/algorithm/include/algorithm/algorithm.hpp
@@ -0,0 +1,10 @@
+/// \copyright Copyright 2019 Silexica GmbH, Lichtstr. 25, Cologne, Germany
+/// All rights reserved.
+/// \file
+/// \brief This file provides includes all common algorithms.
+#ifndef ALGORITHM__ALGORITHM_HPP_
+#define ALGORITHM__ALGORITHM_HPP_
+
+#include <algorithm/quick_sort.hpp>
+
+#endif  // ALGORITHM__ALGORITHM_HPP_
