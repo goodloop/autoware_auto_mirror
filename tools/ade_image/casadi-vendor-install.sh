7900230ddf2ab7ab41cfdc42f7d2bbf28ccf4889
Joshua Whitley <josh.whitley@autoware.org>
2020-03-01 18:35:14 -0600

    [#266] Forgot to install dependencies.

--- tools/ade_image/casadi-vendor-install.sh
+++ tools/ade_image/casadi-vendor-install.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+set -e
+
 ARCH=""
 
 if [ $(uname -m) == 'x86_64' ]; then
@@ -8,6 +11,10 @@ else
 fi
 
 curl -Ls -o /tmp/ros-dashing-casadi-vendor.deb https://gitlab.com/autowarefoundation/autoware.auto/casadi_vendor/-/jobs/artifacts/master/raw/ros-dashing-casadi-vendor_3.5.1-0bionic_$ARCH.deb?job=build_debian_$ARCH
+
+apt-get update
+apt-get install -y coinor-libipopt-dev gfortran liblapack-dev pkg-config git
+rm -rf /var/lib/apt/lists/*
 
 dpkg -i /tmp/ros-dashing-casadi-vendor.deb
 
