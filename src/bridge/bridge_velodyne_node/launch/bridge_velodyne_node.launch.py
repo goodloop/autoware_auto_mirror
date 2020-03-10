4d8fd401f86c33f52fe87078be956a3d98c4d5f9
Joshua Whitley <josh.whitley@autoware.org>
2020-03-09 22:10:02 -0500

    [#333] Changing namespace in launch files to Dashing format.

--- src/bridge/bridge_velodyne_node/launch/bridge_velodyne_node.launch.py
+++ src/bridge/bridge_velodyne_node/launch/bridge_velodyne_node.launch.py
@@ -37,7 +37,7 @@ def generate_launch_description():
     velodyne_node_runner = launch_ros.actions.Node(
         package='velodyne_node',
         node_executable='velodyne_cloud_node_exe',
-        namespace='lidar_front',
+        node_namespace='lidar_front',
         parameters=[param_file])
 
     # ros1 bridge runner definition.
