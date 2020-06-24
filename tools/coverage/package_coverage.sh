4171d94ae9eadef52f4b9c1b118411d0d47ed38b
Jeffrey Kane Johnson <jeff@mapless.ai>
2020-06-24 14:43:27 -0400

    use same commands as in coverage.sh

--- tools/coverage/package_coverage.sh
+++ tools/coverage/package_coverage.sh
@@ -31,56 +31,54 @@ fi
 
 # Build with correct flags
 colcon build \
-  --merge-install \
-  --packages-up-to $1 \
-  --cmake-args \
-		-DCMAKE_BUILD_TYPE=Coverage \
+	--packages-up-to $1 \
 	--ament-cmake-args \
 	  -DCMAKE_CXX_FLAGS="${COVERAGE_FLAGS}" \
 	  -DCMAKE_C_FLAGS="${COVERAGE_FLAGS}" || { echo "Build failed." ; exit 1; }
 
 # Get a zero-coverage baseline
 lcov \
+	--config-file .lcovrc \
 	--base-directory "$(pwd)" \
 	--capture \
-	--initial \
 	--directory "$(pwd)/build/$1" \
 	-o "$(pwd)/lcov/lcov.base" \
- 	--no-external --rc lcov_branch_coverage=1 || { echo "Zero baseline coverage failed."; exit 1; }
+	--initial || { echo "Zero baseline coverage failed."; exit 1; }
 
 # Run unit and integration tests
 colcon test \
-	--merge-install \
   --packages-select $1 \
-	--abort-on-error \
-	--ctest-args -E __rmw_micro_dds_c\|pclint\|copyright\|cppcheck\|cpplint\|flake8\|lint_cmake\|pep257\|uncrustify || { echo "Unit/integration testing failed."; exit 1; }
+	--return-code-on-test-failure || { echo "Unit/integration testing failed."; exit 1; }
 
 # Get coverage
 lcov \
-  --base-directory "$(pwd)" \
-  --capture \
-  --directory "$(pwd)/build/$1" \
-  --output-file "$(pwd)/lcov/lcov.run" \
-  --no-external \
-  --rc lcov_branch_coverage=1 || { echo "Coverage generation failed."; exit 1; }
+	--config-file .lcovrc \
+	--base-directory "$(pwd)" \
+	--capture \
+	--directory "$(pwd)/build/$1" \
+	--output-file "$(pwd)/lcov/lcov.run" || { echo "Coverage generation failed."; exit 1; }
 
 # Combine zero-coverage with coverage information.
 lcov \
+	--config-file .lcovrc \
   -a "$(pwd)/lcov/lcov.base" \
   -a "$(pwd)/lcov/lcov.run" \
-  -o "$(pwd)/lcov/lcov.total" \
-  --rc lcov_branch_coverage=1 || { echo "Coverage combination failed."; exit 1; }
+  -o "$(pwd)/lcov/lcov.total" || { echo "Coverage combination failed."; exit 1; }
 
 # Filter test, build, and install files and generate html
-lcov -r "$(pwd)/lcov/lcov.total" \
-    "*/build/*" \
-    "*/install/*" \
-    "CMakeCCompilerId.c" \
-    "CMakeCXXCompilerId.cpp" \
-    -o "$(pwd)/lcov/lcov.total.filtered" \
-    --rc lcov_branch_coverage=1 || { echo "Filtering failed."; exit 1; }
+lcov --config-file .lcovrc -r "$(pwd)/lcov/lcov.total" \
+	"*/AutowareAuto/install/*" "*/CMakeCCompilerId.c" "*/CMakeCXXCompilerId.cpp" "*_msgs/*" \
+	"*/AutowareAuto/build/mpc_planner/*" "*/AutowareAuto/build/mpc_controller/*" \
+	"*/AutowareAuto/src/external/*" \
+	"*/AutowareAuto/build/recordreplay_planner_actions/*" \
+	-o "$(pwd)/lcov/lcov.total.filtered" || { echo "Filtering failed."; exit 1; }
 
-genhtml -o "$(pwd)/lcov/" "$(pwd)/lcov/lcov.total.filtered" -p "$(pwd)" --legend \
---demangle-cpp --rc genhtml_branch_coverage=1 || { echo "HTML generation failed."; exit 1; }
+genhtml \
+	--config-file .lcovrc \
+	-p "$(pwd)" \
+	--legend \
+	--demangle-cpp \
+	"$(pwd)/lcov/lcov.total.filtered" \
+	-o "$(pwd)/lcov/" || { echo "HTML generation failed."; exit 1; }
 
 echo "Check lcov/index.html to see the coverage report."
