b7a988364f6aa46237126f935fdbd07e97111f5f
Nikolai Morin <nikolai.morin@apex.ai>
2021-01-05 11:10:00 +0100

    Ament_uncrustify

--- src/common/autoware_auto_geometry/test/include/test_spatial_hash.hpp
+++ src/common/autoware_auto_geometry/test/include/test_spatial_hash.hpp
@@ -78,7 +78,7 @@ protected:
 
 // Instantiate tests for given types, add more types here as they are used
 using PointTypesSpatialHash = ::testing::Types<geometry_msgs::msg::Point32>;
-TYPED_TEST_CASE(TypedSpatialHashTest, PointTypesSpatialHash,);
+TYPED_TEST_CASE(TypedSpatialHashTest, PointTypesSpatialHash, );
 /// NOTE: This is the older version due to 1.8.0 of GTest. v1.8.1 uses TYPED_TEST_SUITE
 
 
