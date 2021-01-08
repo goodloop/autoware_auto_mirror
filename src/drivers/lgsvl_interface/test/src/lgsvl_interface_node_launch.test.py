0517dc00d4593362c3974b29e224de592f488a74
Jilada Eccleston <jilada.eccleston@gmail.com>
2021-01-08 14:33:57 +0900

    Add lgsvl_interface launch test

---
+++ src/drivers/lgsvl_interface/test/src/lgsvl_interface_node_launch.test.py
@@ -0,0 +1,89 @@
+# Copyright 2021 the Autoware Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Co-developed by Tier IV, Inc. and Apex.AI, Inc.
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import launch_testing
+
+import os
+import pytest
+import unittest
+
+
+def get_share_file(package_name, file_name):
+    return os.path.join(get_package_share_directory(package_name), file_name)
+
+
+@pytest.mark.launch_test
+def generate_test_description(ready_fn):
+
+    # Declare the parameter files needed for launching the node
+    control_command_param = DeclareLaunchArgument(
+        'control_command',
+        default_value="raw",  # use "raw", "basic" or "high_level"
+        description='command control mode topic name')
+
+    # Default lgsvl_interface params
+    lgsvl_interface_param = DeclareLaunchArgument(
+        'lgsvl_interface_param',
+        default_value=[
+            get_share_file('lgsvl_interface', 'param/lgsvl.param.yaml')
+        ],
+        description='Path to config file for lgsvl interface')
+
+    # The node under test and the checker node that will pass/fail our tests:
+    lgsvl_interface_node = Node(
+        package="lgsvl_interface",
+        node_executable="lgsvl_interface_exe",
+        node_namespace="vehicle",
+        output='screen',
+
+        parameters=[
+            LaunchConfiguration('lgsvl_interface_param'),
+            # overwrite parameters from yaml here
+            {"control_command.name": LaunchConfiguration('control_command')}
+        ],
+        remappings=[
+            ("vehicle_control_cmd", "/lgsvl/vehicle_control_cmd"),
+            ("vehicle_state_cmd", "/lgsvl/vehicle_state_cmd"),
+            ("state_report", "/lgsvl/state_report"),
+            ("state_report_out", "state_report"),
+            ("gnss_odom", "/lgsvl/gnss_odom"),
+            ("vehicle_odom", "/lgsvl/vehicle_odom")
+        ]
+    )
+
+    context = {'lgsvl_interface_node': lgsvl_interface_node}
+
+    return LaunchDescription([
+        control_command_param,
+        lgsvl_interface_param,
+        lgsvl_interface_node,
+        # Start tests right away - no need to wait for anything
+        OpaqueFunction(function=lambda context: ready_fn())]
+    ), context
+
+
+@launch_testing.post_shutdown_test()
+class TestProcessOutput(unittest.TestCase):
+
+    def test_exit_code(self, proc_output, proc_info, lgsvl_interface_node):
+        # Check that process exits with code -15 code: termination request, sent to the program
+        launch_testing.asserts.assertExitCodes(proc_info, [-15], process=lgsvl_interface_node)
