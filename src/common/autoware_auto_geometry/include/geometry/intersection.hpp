1ad96e3548cc05bc5816ad9cff62d5ad67dd2219
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-01-18 19:34:07 +0100

    Fix style

--- src/common/autoware_auto_geometry/include/geometry/intersection.hpp
+++ src/common/autoware_auto_geometry/include/geometry/intersection.hpp
@@ -139,7 +139,9 @@ bool intersect(const Iter begin1, const Iter end1, const Iter begin2, const Iter
     auto minmax_2 = get_projected_min_max(begin2, end2);
 
     // Check for any intersections
-    if (minmax_1.first > minmax_2.second + autoware::common::types::FEPS || minmax_2.first > minmax_1.second + autoware::common::types::FEPS) {
+    if (minmax_1.first > minmax_2.second + autoware::common::types::FEPS ||
+      minmax_2.first > minmax_1.second + autoware::common::types::FEPS)
+    {
       // Found separating hyperplane, stop
       return false;
     }
