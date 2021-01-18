cbb4fc8cfc772525b5f9de3428db0143790770b1
Jilada Eccleston <jilada.eccleston@gmail.com>
2021-01-18 13:38:10 +0900

    Fix ssc_interface launch tests  - Add missing package dependencies  - Remove unnecessary launch arguments

--- src/drivers/ssc_interface/test/ssc_interface_launch.test.py
+++ src/drivers/ssc_interface/test/ssc_interface_launch.test.py
@@ -16,7 +16,6 @@
 
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.actions import OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -33,18 +32,6 @@ def get_share_file(package_name, file_name):
 
 @pytest.mark.launch_test
 def generate_test_description(ready_fn):
-    # Prepare parameters as per launch file:
-    control_command_param = DeclareLaunchArgument(
-        'control_command',
-        default_value="basic",  # use "raw", "basic" or "high_level"
-        description='command control mode')
-
-    ssc_interface_param = DeclareLaunchArgument(
-        'ssc_interface_param',
-        default_value=[
-            get_share_file('ssc_interface', 'param/defaults.param.yaml')
-        ],
-        description='Path to config file for SSC interface')
 
     # Prepare node
     ssc_interface = Node(
@@ -53,27 +40,14 @@ def generate_test_description(ready_fn):
         node_executable='ssc_interface_node_exe',
         node_namespace='vehicle',
         output='screen',
-        parameters=[LaunchConfiguration('ssc_interface_param')],
-        remappings=[
-            ('gear_select', '/ssc/gear_select'),
-            ('arbitrated_speed_commands', '/ssc/arbitrated_speed_commands'),
-            ('arbitrated_steering_commands', '/ssc/arbitrated_steering_commands'),
-            ('turn_signal_command', '/ssc/turn_signal_command'),
-            ('dbw_enabled_feedback', '/ssc/dbw_enabled_fedback'),
-            ('gear_feedback', '/ssc/gear_feedback'),
-            ('velocity_accel_cov', '/ssc/velocity_accel_cov'),
-            ('steering_feedback', '/ssc/steering_feedback'),
-            ('vehicle_kinematic_state_cog', '/vehicle/vehicle_kinematic_state'),
-            ('state_report_out', '/vehicle/vehicle_state_report'),
-            ('state_command', '/vehicle/vehicle_state_command')
-        ]
+        parameters=[LaunchConfiguration('ssc_interface_param', default=[
+            get_share_file('ssc_interface', 'param/defaults.param.yaml')
+        ])]
     )
 
     context = {'ssc_interface': ssc_interface}
 
     return LaunchDescription([
-        control_command_param,
-        ssc_interface_param,
         ssc_interface,
         # Start tests right away - no need to wait for anything
         OpaqueFunction(function=lambda context: ready_fn())]
