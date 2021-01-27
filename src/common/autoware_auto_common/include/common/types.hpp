f4029e361c62e8e44007cf14faeb30701a3222f2
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-01-27 15:59:03 +0100

    Use a more precise epsilon for double precision floats

--- src/common/autoware_auto_common/include/common/types.hpp
+++ src/common/autoware_auto_common/include/common/types.hpp
@@ -45,8 +45,8 @@ constexpr float32_t PI_2 = 1.5707963267948966F;
 constexpr float32_t TAU = 6.283185307179586476925286766559F;
 /// arbitrary small constant: 1.0E-6F
 constexpr float32_t FEPS = 0.000001F;
-/// arbitrary small constant: 1.0E-15
-constexpr float64_t DEPS = 0.000000000000001;
+/// arbitrary small constant: 1.0E-16
+constexpr float64_t DEPS = 0.0000000000000001;
 
 struct COMMON_PUBLIC PointXYZIF
 {
