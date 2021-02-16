fdbb2bf4313463680993a416d33218395eb7a7d2
mitsudome-r <ryohsuke.mitsudome@tier4.jp>
2021-02-17 07:53:41 +0900

    use PI_2 from autoware::common::types for pi/2
    Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

--- src/planning/recordreplay_planner_nodes/test/sanity_checks.cpp
+++ src/planning/recordreplay_planner_nodes/test/sanity_checks.cpp
@@ -65,7 +65,9 @@ TEST(mytest_base, basic)
   node_options_rr.append_parameter_override("enable_object_collision_estimator", true);
   node_options_rr.append_parameter_override("enable_object_collision_estimator", true);
   node_options_rr.append_parameter_override("goal_distance_threshold_m", 0.75);
-  node_options_rr.append_parameter_override("goal_angle_threshold_rad", 1.54);
+  node_options_rr.append_parameter_override(
+    "goal_angle_threshold_rad",
+    autoware::common::types::PI_2);
   auto plannernode = std::make_shared<RecordReplayPlannerNode>(node_options_rr);
 
   using PubAllocT = rclcpp::PublisherOptionsWithAllocator<std::allocator<void>>;
