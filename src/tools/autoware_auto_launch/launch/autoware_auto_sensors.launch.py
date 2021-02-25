b590493be4238ed6aa956bf7373cde7ac77b8011
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-02-25 18:57:31 +0900

    Add sensors launch file  - These launch files are associated with transforming the raw sensor output

--- src/tools/autoware_auto_launch/launch/autoware_auto_sensors.launch.py
+++ src/tools/autoware_auto_launch/launch/autoware_auto_sensors.launch.py
@@ -11,3 +11,95 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+import os
+
+
+def generate_launch_description():
+    """
+    Launch sensors
+     * point_cloud_filter_transform
+     * point_cloud_fusion_node
+     * voxel_grid_node
+    """
+    autoware_auto_launch_pkg_prefix = get_package_share_directory(
+        'autoware_auto_launch')
+    point_cloud_filter_transform_param_file = os.path.join(
+        autoware_auto_launch_pkg_prefix, 'param/point_cloud_filter_transform.param.yaml')
+    point_cloud_fusion_node_param_file = os.path.join(
+        autoware_auto_launch_pkg_prefix, 'param/point_cloud_fusion.param.yaml')
+    voxel_grid_node_param_file = os.path.join(
+        autoware_auto_launch_pkg_prefix, 'param/voxel_grid_node.param.yaml')
+
+    # Arguments
+    point_cloud_filter_transform_param = DeclareLaunchArgument(
+        'point_cloud_filter_transform_param_file',
+        default_value=point_cloud_filter_transform_param_file,
+        description='Path to config file for Point Cloud Filter/Transform Nodes'
+    )
+    point_cloud_fusion_node_param = DeclareLaunchArgument(
+        'point_cloud_fusion_node_param_file',
+        default_value=point_cloud_fusion_node_param_file,
+        description='Path to config file for Point Cloud Fusion Nodes'
+    )
+    voxel_grid_node_param = DeclareLaunchArgument(
+        'voxel_grid_node_param_file',
+        default_value=voxel_grid_node_param_file,
+        description='Path to config file for lidar scan downsampler'
+    )
+
+    # Nodes
+    filter_transform_vlp16_front = Node(
+        package='point_cloud_filter_transform_nodes',
+        node_executable='point_cloud_filter_transform_node_exe',
+        node_name='filter_transform_vlp16_front',
+        node_namespace='lidar_front',
+        parameters=[LaunchConfiguration('point_cloud_filter_transform_param_file')],
+        remappings=[("points_in", "points_raw")]
+    )
+    filter_transform_vlp16_rear = Node(
+        package='point_cloud_filter_transform_nodes',
+        node_executable='point_cloud_filter_transform_node_exe',
+        node_name='filter_transform_vlp16_rear',
+        node_namespace='lidar_rear',
+        parameters=[LaunchConfiguration('point_cloud_filter_transform_param_file')],
+        remappings=[("points_in", "points_raw")]
+    )
+    point_cloud_fusion_node = Node(
+        package='point_cloud_fusion_nodes',
+        node_executable='pointcloud_fusion_node_exe',
+        node_namespace='lidars',
+        parameters=[LaunchConfiguration('point_cloud_fusion_node_param_file')],
+        remappings=[
+            ("output_topic", "points_fused"),
+            ("input_topic1", "/lidar_front/points_filtered"),
+            ("input_topic2", "/lidar_rear/points_filtered")
+        ]
+    )
+    voxel_grid_node = Node(
+        package='voxel_grid_nodes',
+        node_executable='voxel_grid_node_exe',
+        node_namespace='lidars',
+        node_name='voxel_grid_cloud_node',
+        parameters=[LaunchConfiguration('voxel_grid_node_param_file')],
+        remappings=[
+            ("points_in", "points_fused"),
+            ("points_downsampled", "points_fused_downsampled")
+        ]
+    )
+
+    return LaunchDescription([
+        point_cloud_filter_transform_param,
+        point_cloud_fusion_node_param,
+        voxel_grid_node_param,
+        filter_transform_vlp16_front,
+        filter_transform_vlp16_rear,
+        voxel_grid_node,
+        point_cloud_fusion_node,
+    ])
