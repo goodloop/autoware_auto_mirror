26faa02e6ee2d454fe3e3159869fe01bff12a080
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-02-25 16:59:24 +0900

    Add mapping launch file  - Add mapping parameter files

--- src/tools/autoware_auto_launch/launch/autoware_auto_mapping.launch.py
+++ src/tools/autoware_auto_launch/launch/autoware_auto_mapping.launch.py
@@ -11,3 +11,66 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+import os
+
+
+def generate_launch_description():
+    """
+    Launch mapping nodes
+     * lanelet2_map_provider/visualizer
+     * map_provider
+    """
+    autoware_auto_launch_pkg_prefix = get_package_share_directory(
+        'autoware_auto_launch')
+    lanelet2_map_provider_param_file = os.path.join(
+        autoware_auto_launch_pkg_prefix, 'param/lanelet2_map_provider.param.yaml')
+    map_publisher_param_file = os.path.join(
+        autoware_auto_launch_pkg_prefix, 'param/map_publisher.param.yaml')
+
+    # Arguments
+    lanelet2_map_provider_param = DeclareLaunchArgument(
+        'lanelet2_map_provider_param_file',
+        default_value=lanelet2_map_provider_param_file,
+        description='Path to parameter file for Lanelet2 Map Provider'
+    )
+    map_publisher_param = DeclareLaunchArgument(
+        'map_publisher_param_file',
+        default_value=map_publisher_param_file,
+        description='Path to config file for Map Publisher'
+    )
+
+    # Nodes
+    lanelet2_map_provider = Node(
+        package='lanelet2_map_provider',
+        node_executable='lanelet2_map_provider_exe',
+        node_namespace='had_maps',
+        node_name='lanelet2_map_provider_node',
+        parameters=[LaunchConfiguration('lanelet2_map_provider_param_file')]
+    )
+    lanelet2_map_visualizer = Node(
+        package='lanelet2_map_provider',
+        node_executable='lanelet2_map_visualizer_exe',
+        node_name='lanelet2_map_visualizer_node',
+        node_namespace='had_maps'
+    )
+    map_publisher = Node(
+        package='ndt_nodes',
+        node_executable='ndt_map_publisher_exe',
+        node_namespace='localization',
+        parameters=[LaunchConfiguration('map_publisher_param_file')]
+    )
+
+    return LaunchDescription([
+        lanelet2_map_provider_param,
+        map_publisher_param,
+        lanelet2_map_provider,
+        lanelet2_map_visualizer,
+        map_publisher,
+    ])
