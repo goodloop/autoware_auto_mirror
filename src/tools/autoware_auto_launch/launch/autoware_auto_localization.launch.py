270b77342c0f73c17c824cdaed972a0a78b36826
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-02-26 11:22:57 +0900

    Add localization launch file

--- src/tools/autoware_auto_launch/launch/autoware_auto_localization.launch.py
+++ src/tools/autoware_auto_launch/launch/autoware_auto_localization.launch.py
@@ -11,3 +11,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+import os
+
+
+def generate_launch_description():
+    """
+    Launch localization
+     * ndt_localizer
+    """
+    autoware_auto_launch_pkg_prefix = get_package_share_directory(
+        'autoware_auto_launch')
+    ndt_localizer_param_file = os.path.join(
+        autoware_auto_launch_pkg_prefix, 'param/ndt_localizer.param.yaml')
+
+    # Arguments
+    ndt_localizer_param = DeclareLaunchArgument(
+        'ndt_localizer_param_file',
+        default_value=ndt_localizer_param_file,
+        description='Path to config file for ndt localizer'
+    )
+
+    # Nodes
+    ndt_localizer = Node(
+        package='ndt_nodes',
+        node_executable='p2d_ndt_localizer_exe',
+        node_namespace='localization',
+        node_name='p2d_ndt_localizer_node',
+        parameters=[LaunchConfiguration('ndt_localizer_param_file')],
+        remappings=[
+            ("points_in", "/lidars/points_fused_downsampled"),
+            ("observation_republish", "/lidars/points_fused_viz"),
+        ]
+    )
+
+    return LaunchDescription([
+        ndt_localizer_param,
+        ndt_localizer,
+    ])
