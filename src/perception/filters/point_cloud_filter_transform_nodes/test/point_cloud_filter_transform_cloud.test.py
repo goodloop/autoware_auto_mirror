daa74857e7ecef1c9f3db7d8755aa52fb6349c3c
Divya Aggarwal <divya.aggarwal@apex.ai>
2021-03-18 18:13:11 -0700

    Add integration test for tf publisher and remove gtest

--- src/perception/filters/point_cloud_filter_transform_nodes/test/point_cloud_filter_transform_cloud.test.py
+++ src/perception/filters/point_cloud_filter_transform_nodes/test/point_cloud_filter_transform_cloud.test.py
@@ -74,6 +74,7 @@ def generate_test_description(ready_fn):
         size_tolerance=1.0
     )
 
+    # Fallback on data from transform publisher if transform not found in test.param.yaml
     lidar_bl_publisher = launch_ros.actions.Node(
         package='tf2_ros',
         node_executable='static_transform_publisher',
