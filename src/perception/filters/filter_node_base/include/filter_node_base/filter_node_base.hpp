e36edf7b55ad593894323e15ce6d9f7a588afdf8
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-04-05 19:41:04 +0900

    Make compile (tests still fail - there aren't any!)  - Fix Werror errors and use common types  - Remove references to pcl_ros (for now)

--- src/perception/filters/filter_node_base/include/filter_node_base/filter_node_base.hpp
+++ src/perception/filters/filter_node_base/include/filter_node_base/filter_node_base.hpp
@@ -21,7 +21,35 @@
 
 #include <filter_node_base/visibility_control.hpp>
 
-#include <rclcpp/rclcpp.hpp>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <common/types.hpp>
+
+// PCL includes
+#include "pcl/filters/filter.h"
+
+#include "sensor_msgs/msg/point_cloud2.h"
+#include "boost/thread/mutex.hpp"
+// PCL includes
+#include "pcl/pcl_base.h"
+#include "pcl/point_types.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include "pcl_msgs/msg/model_coefficients.h"
+#include "pcl_msgs/msg/point_indices.h"
+
+#include "message_filters/subscriber.h"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/sync_policies/exact_time.h"
+#include "message_filters/synchronizer.h"
+
+// Include TF
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/create_timer_ros.h"
+#include "tf2_ros/transform_listener.h"
+
+using autoware::common::types::bool8_t;
 
 namespace autoware
 {
@@ -32,17 +60,187 @@ namespace filters
 namespace filter_node_base
 {
 
+namespace sync_policies = message_filters::sync_policies;
+
+/** \brief For parameter service callback */
+template<typename T>
+bool get_param(const std::vector<rclcpp::Parameter> & p, const std::string & name, T & value)
+{
+  auto it = std::find_if(
+    p.cbegin(), p.cend(), [&name](const rclcpp::Parameter & parameter) {
+      return parameter.get_name() == name;
+    });
+  if (it != p.cend()) {
+    value = it->template get_value<T>();
+    return true;
+  }
+  return false;
+}
+
 /// \class FilterNodeBase
 /// \brief ROS 2 Node for hello world.
 class FILTER_NODE_BASE_PUBLIC FilterNodeBase : public rclcpp::Node
 {
 public:
-  /// \brief default constructor, starts driver
-  /// \throw runtime error if failed to start threads or configure driver
-  explicit FilterNodeBase(const rclcpp::NodeOptions & options);
+  typedef sensor_msgs::msg::PointCloud2 PointCloud2;
+  typedef sensor_msgs::msg::PointCloud2::ConstSharedPtr PointCloud2ConstPtr;
+
+  typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+  typedef PointCloud::Ptr PointCloudPtr;
+  typedef PointCloud::ConstPtr PointCloudConstPtr;
+
+  typedef pcl_msgs::msg::PointIndices PointIndices;
+  typedef PointIndices::SharedPtr PointIndicesPtr;
+  typedef PointIndices::ConstSharedPtr PointIndicesConstPtr;
+
+  typedef pcl_msgs::msg::ModelCoefficients ModelCoefficients;
+  typedef ModelCoefficients::SharedPtr ModelCoefficientsPtr;
+  typedef ModelCoefficients::ConstSharedPtr ModelCoefficientsConstPtr;
+
+  typedef pcl::IndicesPtr IndicesPtr;
+  typedef pcl::IndicesConstPtr IndicesConstPtr;
+
+  typedef message_filters::Synchronizer<sync_policies::ExactTime<PointCloud2, PointIndices>>
+    ExactTimeSyncPolicy;
+  typedef message_filters::Synchronizer<sync_policies::ApproximateTime<PointCloud2, PointIndices>>
+    ApproximateTimeSyncPolicy;
+
+  /// \brief default constructor for the FilterNodeBase class
+  explicit FilterNodeBase(const std::string & filter_name = "pointcloud_preprocessor_filter",
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+protected:
+  /** \brief The input PointCloud2 subscriber. */
+  rclcpp::Subscription<PointCloud2>::SharedPtr sub_input_;
+
+  /** \brief The output PointCloud2 publisher. */
+  rclcpp::Publisher<PointCloud2>::SharedPtr pub_output_;
+
+  /** \brief The message filter subscriber for PointCloud2. */
+  message_filters::Subscriber<PointCloud2> sub_input_filter_;
+
+  /** \brief The message filter subscriber for PointIndices. */
+  message_filters::Subscriber<PointIndices> sub_indices_filter_;
+
+  /** \brief The desired user filter field name. */
+  std::string filter_field_name_;
+
+  /** \brief The minimum allowed filter value a point will be considered from. */
+  double filter_limit_min_;
+
+  /** \brief The maximum allowed filter value a point will be considered from. */
+  double filter_limit_max_;
+
+  /** \brief Set to true if we want to return the data outside (\a filter_limit_min_;\a filter_limit_max_).
+   * Default: false. */
+  bool filter_limit_negative_;
+
+  /** \brief The input TF frame the data should be transformed into,
+   * if input.header.frame_id is different. */
+  std::string tf_input_frame_;
+
+  /** \brief The original data input TF frame. */
+  std::string tf_input_orig_frame_;
+
+  /** \brief The output TF frame the data should be transformed into,
+   * if input.header.frame_id is different. */
+  std::string tf_output_frame_;
+
+  /** \brief Internal mutex. */
+  boost::mutex mutex_;
+
+  /** \brief Virtual abstract filter method. To be implemented by every child.
+   * \param input the input point cloud dataset.
+   * \param indices a pointer to the vector of point indices to use.
+   * \param output the resultant filtered PointCloud2
+   */
+  virtual void filter(
+    const PointCloud2ConstPtr & input, const IndicesPtr & indices, PointCloud2 & output) = 0;
+
+  /** \brief Call the child filter () method, optionally transform the result, and publish it.
+   * \param input the input point cloud dataset.
+   * \param indices a pointer to the vector of point indices to use.
+   */
+  void computePublish(const PointCloud2ConstPtr & input, const IndicesPtr & indices);
+
+  //////////////////////
+  // from PCLNodelet //
+  //////////////////////
+  /** \brief Set to true if point indices are used.
+   *
+   * When receiving a point cloud, if use_indices_ is false, the entire
+   * point cloud is processed for the given operation. If use_indices_ is
+   * true, then the ~indices topic is read to get the vector of point
+   * indices specifying the subset of the point cloud that will be used for
+   * the operation. In the case where use_indices_ is true, the ~input and
+   * ~indices topics must be synchronised in time, either exact or within a
+   * specified jitter. See also @ref latched_indices_ and approximate_sync.
+   **/
+  bool8_t use_indices_ = false;
+  /** \brief Set to true if the indices topic is latched.
+   *
+   * If use_indices_ is true, the ~input and ~indices topics generally must
+   * be synchronised in time. By setting this flag to true, the most recent
+   * value from ~indices can be used instead of requiring a synchronised
+   * message.
+   **/
+  bool8_t latched_indices_ = false;
+
+  /** \brief The maximum queue size (default: 3). */
+  size_t max_queue_size_ = 3;
+
+  /** \brief True if we use an approximate time synchronizer
+   * versus an exact one (false by default). */
+  bool8_t approximate_sync_ = false;
+
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
+  inline bool isValid(const PointCloud2ConstPtr & cloud, const std::string & topic_name = "input")
+  {
+    if (cloud->width * cloud->height * cloud->point_step != cloud->data.size()) {
+      RCLCPP_WARN(
+        this->get_logger(),
+        "Invalid PointCloud (data = %zu, width = %d, height = %d, step = %d) with stamp %f, "
+        "and frame %sreceived!",
+        cloud->data.size(), cloud->width, cloud->height, cloud->point_step,
+        rclcpp::Time(cloud->header.stamp).seconds(), cloud->header.frame_id.c_str());
+      return false;
+    }
+    return true;
+  }
+
+  inline bool isValid(
+    const PointIndicesConstPtr & /*indices*/, const std::string & /*topic_name*/ = "indices")
+  {
+    return true;
+  }
+
+  inline bool isValid(
+    const ModelCoefficientsConstPtr & /*model*/, const std::string & /*topic_name*/ = "model")
+  {
+    return true;
+  }
 
 private:
-  bool verbose;  ///< whether to use verbose output or not.
+  /** \brief Parameter service callback result : needed to be hold */
+  OnSetParametersCallbackHandle::SharedPtr set_param_res_filter_;
+
+  /** \brief Parameter service callback */
+  rcl_interfaces::msg::SetParametersResult filterParamCallback(
+    const std::vector<rclcpp::Parameter> & p);
+
+  /** \brief Synchronized input, and indices.*/
+  std::shared_ptr<ExactTimeSyncPolicy> sync_input_indices_e_;
+  std::shared_ptr<ApproximateTimeSyncPolicy> sync_input_indices_a_;
+
+  /** \brief PointCloud2 + Indices data callback. */
+  void input_indices_callback(const PointCloud2ConstPtr cloud, const PointIndicesConstPtr indices);
+
+  void setupTF();
+
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
 }  // namespace filter_node_base
 }  // namespace filters
