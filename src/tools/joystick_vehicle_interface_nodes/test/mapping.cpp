5982aec1f8ea0d3ac85315e8b189f4225f83d504
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-04-12 13:06:15 +0200

    More replacements

--- src/tools/joystick_vehicle_interface_nodes/test/mapping.cpp
+++ src/tools/joystick_vehicle_interface_nodes/test/mapping.cpp
@@ -348,14 +348,14 @@ TEST_P(joy_vi_test, basic_mapping)
       (!button_check_fn(Buttons::HAND_BRAKE_TOGGLE) || state.msg_->hand_brake) &&
       (button_check_fn(Buttons::AUTONOMOUS_TOGGLE) == (state.msg_->mode == VSC::MODE_AUTONOMOUS)) &&
       (button_check_fn(Buttons::HEADLIGHTS_TOGGLE) ==
-      (state.msg_->headlight == HeadlightsCommand::ON)) &&
+      (state.msg_->headlight == HeadlightsCommand::ENABLE_LOW)) &&
       (button_check_fn(Buttons::WIPER_TOGGLE) == (state.msg_->wiper == VSC::WIPER_LOW));
     const auto toggle_case2 =
       (!button_check_fn(Buttons::HORN_TOGGLE) || !state.msg_->horn) &&
       (!button_check_fn(Buttons::HAND_BRAKE_TOGGLE) || !state.msg_->hand_brake) &&
       (button_check_fn(Buttons::AUTONOMOUS_TOGGLE) == (state.msg_->mode == VSC::MODE_MANUAL)) &&
       (button_check_fn(Buttons::HEADLIGHTS_TOGGLE) ==
-      (state.msg_->headlight == HeadlightsCommand::OFF)) &&
+      (state.msg_->headlight == HeadlightsCommand::DISABLE)) &&
       (button_check_fn(Buttons::WIPER_TOGGLE) == (state.msg_->wiper == VSC::WIPER_OFF));
     if (toggle_case1 || toggle_case2) {
       EXPECT_TRUE(true);  // Pushed logic into conditional for printing purposes
@@ -375,8 +375,8 @@ TEST_P(joy_vi_test, basic_mapping)
         "mode", state.msg_->mode, VSC::MODE_AUTONOMOUS, VSC::MODE_MANUAL,
         Buttons::AUTONOMOUS_TOGGLE);
       err_print(
-        "headlight", state.msg_->headlight, HeadlightsCommand::ON,
-        HeadlightsCommand::OFF, Buttons::HEADLIGHTS_TOGGLE);
+        "headlight", state.msg_->headlight, HeadlightsCommand::ENABLE_LOW,
+        HeadlightsCommand::DISABLE, Buttons::HEADLIGHTS_TOGGLE);
       err_print("wiper", state.msg_->wiper, VSC::WIPER_LOW, VSC::WIPER_OFF, Buttons::WIPER_TOGGLE);
       EXPECT_TRUE(false);
     }
