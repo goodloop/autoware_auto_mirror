b3feb974e4b813345b45aa978573e8246026e086
Jilada Eccleston <jilada.eccleston@gmail.com>
2021-05-10 16:25:00 +0900

    Update copyright to Tier IV

--- src/perception/filters/filter_node_base/include/filter_node_base/filter_node_base.hpp
+++ src/perception/filters/filter_node_base/include/filter_node_base/filter_node_base.hpp
@@ -1,4 +1,4 @@
-// Copyright 2021 The Autoware Foundation
+// Copyright 2021 Tier IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// \copyright Copyright 2021 The Autoware Foundation
+/// \copyright Copyright 2021 Tier IV, Inc.
 /// \file
 /// \brief This file defines the FilterNodeBase class.
 
