0e7c1cd771a936da4a64d5da0ebaabc7b91a6edc
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-05-16 23:31:08 +0200

    Added handling of HeadlightsCommands

--- src/drivers/lgsvl_interface/include/lgsvl_interface/lgsvl_interface.hpp
+++ src/drivers/lgsvl_interface/include/lgsvl_interface/lgsvl_interface.hpp
@@ -129,6 +129,9 @@ public:
   /// Send wipers command data.
   void send_wipers_command(const autoware_auto_msgs::msg::WipersCommand & msg) override;
 
+  void on_headlights_command(
+    const autoware_auto_msgs::msg::HeadlightsCommand &) override;
+
 private:
   // Mappings from Autoware to LGSVL values
   static const std::unordered_map<WIPER_TYPE, WIPER_TYPE> autoware_to_lgsvl_wiper;
@@ -152,6 +155,8 @@ private:
   rclcpp::Subscription<lgsvl_msgs::msg::CanBusData>::SharedPtr m_state_sub{};
   rclcpp::Subscription<lgsvl_msgs::msg::VehicleOdometry>::SharedPtr m_veh_odom_sub{};
   rclcpp::TimerBase::SharedPtr m_nav_base_tf_timer{};
+
+  rclcpp::Publisher<autoware_auto_msgs::msg::HeadlightsCommand>::SharedPtr m_headlights_command_pub;
 
   Table1D m_throttle_table;
   Table1D m_brake_table;
