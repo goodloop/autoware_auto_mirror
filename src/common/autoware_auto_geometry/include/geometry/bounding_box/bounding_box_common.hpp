ub-cavas/AutowareAuto

[#1064] Add option to publish DetectedObjects from euclidean_cluster_nodes

src/common/autoware_auto_geometry/include/geometry/bounding_box/bounding_box_common.hpp
@@ -21,8 +21,11 @@
 #define GEOMETRY__BOUNDING_BOX__BOUNDING_BOX_COMMON_HPP_
 
 #include <autoware_auto_msgs/msg/bounding_box.hpp>
+#include <autoware_auto_msgs/msg/detected_object.hpp>
+#include <autoware_auto_msgs/msg/shape.hpp>
 #include <geometry/visibility_control.hpp>
 #include <geometry/common_2d.hpp>
+#include <geometry_msgs/msg/pose.hpp>
 #include <array>
 #include <limits>
 
@@ -62,6 +65,32 @@ void compute_height(const IT begin, const IT end, BoundingBox & box)
     corner.z = box.centroid.z;
   }
   box.size.z = (max_z - min_z) * 0.5F;
+}
+
+/// \brief Computes height of bounding box given a full list of points
+/// \param[in] begin The start of the list of points
+/// \param[in] end One past the end of the list of points
+/// \param[out] shape A shape in which vertices z values and height field will be set
+/// \tparam IT An iterator type, must dereference into a point type with float member z, or
+///            appropriate point adapter defined
+template<typename IT>
+void compute_height(const IT begin, const IT end, autoware_auto_msgs::msg::Shape & shape)
+{
+  float32_t max_z = -std::numeric_limits<float32_t>::max();
+  float32_t min_z = std::numeric_limits<float32_t>::max();
+  for (auto it = begin; it != end; ++it) {
+    const float32_t z = point_adapter::z_(*it);
+    if (z <= min_z) {
+      min_z = z;
+    }
+    if (z >= max_z) {
+      max_z = z;
+    }
+  }
+  for (auto & corner : shape.polygon.points) {
+    corner.z = min_z;
+  }
+  shape.height = max_z - min_z;
 }
 
 namespace details
@@ -123,6 +152,22 @@ void compute_corners(
   }
 }
 // TODO(c.ho) type trait enum base
+
+/// \brief Copy vertices of the given box into a Shape type
+/// \param box box to be converted
+/// \return Shape type filled with box vertices
+autoware_auto_msgs::msg::Shape GEOMETRY_PUBLIC get_shape(const BoundingBox & box);
+
+/// \brief Copy centroid and orientation info of the box into Pose type
+/// \param box BoundingBox to be converted
+/// \return Pose type filled with centroid and orientation from box
+geometry_msgs::msg::Pose GEOMETRY_PUBLIC get_pose(const BoundingBox & box);
+
+/// \brief Fill DetectedObject type with contents from a BoundingBox type
+/// \param box BoundingBox to be converted
+/// \return Filled DetectedObject type
+autoware_auto_msgs::msg::DetectedObject GEOMETRY_PUBLIC get_detected_object(
+  const BoundingBox & box);
 
 }  // namespace details
 }  // namespace bounding_box

src/common/autoware_auto_geometry/src/bounding_box.cpp
@@ -23,6 +23,7 @@
 #include <algorithm>
 #include <limits>
 #include <list>
+#include <utility>
 #include <vector>
 
 namespace autoware
@@ -63,8 +64,55 @@ void finalize_box(const decltype(BoundingBox::corners) & corners, BoundingBox & 
   // centroid
   box.centroid = times_2d(plus_2d(corners[0U], corners[2U]), 0.5F);
 }
+
+
+autoware_auto_msgs::msg::Shape get_shape(const BoundingBox & box)
+{
+  autoware_auto_msgs::msg::Shape ret;
+  for (const auto & corner_pt : box.corners) {
+    ret.polygon.points.emplace_back(corner_pt);
+  }
+  return ret;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+geometry_msgs::msg::Pose get_pose(const BoundingBox & box)
+{
+  geometry_msgs::msg::Pose ret;
+  ret.position.x = static_cast<double>(box.centroid.x);
+  ret.position.y = static_cast<double>(box.centroid.y);
+  ret.position.z = static_cast<double>(box.centroid.z);
+
+  ret.orientation.x = static_cast<double>(box.orientation.x);
+  ret.orientation.y = static_cast<double>(box.orientation.y);
+  ret.orientation.z = static_cast<double>(box.orientation.z);
+  ret.orientation.w = static_cast<double>(box.orientation.w);
+
+  return ret;
+}
+
+autoware_auto_msgs::msg::DetectedObject get_detected_object(const BoundingBox & box)
+{
+  autoware_auto_msgs::msg::DetectedObject ret;
+
+  ret.kinematics.has_pose = true;
+  ret.kinematics.pose.pose = get_pose(box);
+
+  ret.shape = get_shape(box);
+
+  ret.existence_probability = 1.0F;
+
+  autoware_auto_msgs::msg::ObjectClassification label;
+  label.classification = autoware_auto_msgs::msg::ObjectClassification::UNKNOWN;
+  label.probability = 1.0F;
+  ret.classification.emplace_back(std::move(label));
+
+  return ret;
+}
+
 }  // namespace details
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // precompilation
 using autoware::common::types::PointXYZIF;
 template BoundingBox minimum_area_bounding_box<PointXYZIF>(std::list<PointXYZIF> & list);

src/perception/segmentation/euclidean_cluster/src/shape_estimation.cpp
@@ -1,0 +1,204 @@
+// Copyright 2021 Apex.AI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <euclidean_cluster/shape_estimation.hpp>
+#include <lidar_utils/point_cloud_utils.hpp>
+#include <geometry/bounding_box_2d.hpp>
+
+namespace autoware
+{
+namespace perception
+{
+namespace segmentation
+{
+namespace euclidean_cluster
+{
+
+////////////////////////////////////////////////////////////////////////////////
+ComputeLfitBoundingBoxes::ComputeLfitBoundingBoxes(
+  const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeLfitBoundingBoxes::operator()(
+  autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::get_detected_object(
+          common::geometry::bounding_box::lfit_bounding_box_2d(
+            iter_pair.first, iter_pair.second)));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeLfitBoundingBoxes::operator()(autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::lfit_bounding_box_2d(iter_pair.first, iter_pair.second));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+ComputeLfitBoundingBoxesWithZ::ComputeLfitBoundingBoxesWithZ(
+  const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeLfitBoundingBoxesWithZ::operator()(
+  autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::get_detected_object(
+          common::geometry::bounding_box::lfit_bounding_box_2d(
+            iter_pair.first,
+            iter_pair.second)));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, objects_msg.objects.back().shape);
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeLfitBoundingBoxesWithZ::operator()(autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::lfit_bounding_box_2d(iter_pair.first, iter_pair.second));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, boxes.boxes.back());
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+ComputeEigenBoxes::ComputeEigenBoxes(const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeEigenBoxes::operator()(autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::get_detected_object(
+          common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second)));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeEigenBoxes::operator()(autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+ComputeEigenBoxesWithZ::ComputeEigenBoxesWithZ(
+  const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeEigenBoxesWithZ::operator()(
+  autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::get_detected_object(
+          common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second)));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, objects_msg.objects.back().shape);
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeEigenBoxesWithZ::operator()(
+  autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, boxes.boxes.back());
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+}  // namespace euclidean_cluster
+}  // namespace segmentation
+}  // namespace perception
+}  // namespace autoware

goodloop/autoware_auto_mirror

src/common/autoware_auto_geometry/include/geometry/bounding_box/bounding_box_common.hpp
@@ -21,8 +21,11 @@
 #define GEOMETRY__BOUNDING_BOX__BOUNDING_BOX_COMMON_HPP_
 
 #include <autoware_auto_msgs/msg/bounding_box.hpp>
+#include <autoware_auto_msgs/msg/detected_object.hpp>
+#include <autoware_auto_msgs/msg/shape.hpp>
 #include <geometry/visibility_control.hpp>
 #include <geometry/common_2d.hpp>
+#include <geometry_msgs/msg/pose.hpp>
 #include <array>
 #include <limits>
 
@@ -62,6 +65,32 @@ void compute_height(const IT begin, const IT end, BoundingBox & box)
     corner.z = box.centroid.z;
   }
   box.size.z = (max_z - min_z) * 0.5F;
+}
+
+/// \brief Computes height of bounding box given a full list of points
+/// \param[in] begin Iterator pointing to the start of the range of points
+/// \param[in] end Iterator pointing the the end of the range of points
+/// \param[out] shape A shape in which vertices z values and height field will be set
+/// \tparam IT An iterator type, must dereference into a point type with float member z, or
+///            appropriate point adapter defined
+template<typename IT>
+void compute_height(const IT begin, const IT end, autoware_auto_msgs::msg::Shape & shape)
+{
+  float32_t max_z = -std::numeric_limits<float32_t>::max();
+  float32_t min_z = std::numeric_limits<float32_t>::max();
+  for (auto it = begin; it != end; ++it) {
+    const float32_t z = point_adapter::z_(*it);
+    if (z <= min_z) {
+      min_z = z;
+    }
+    if (z >= max_z) {
+      max_z = z;
+    }
+  }
+  for (auto & corner : shape.polygon.points) {
+    corner.z = min_z;
+  }
+  shape.height = max_z - min_z;
 }
 
 namespace details
@@ -123,6 +152,22 @@ void compute_corners(
   }
 }
 // TODO(c.ho) type trait enum base
+
+/// \brief Copy vertices of the given box into a Shape type
+/// \param box Box to be converted
+/// \return Shape type filled with box vertices
+autoware_auto_msgs::msg::Shape GEOMETRY_PUBLIC make_shape(const BoundingBox & box);
+
+/// \brief Copy centroid and orientation info of the box into Pose type
+/// \param box BoundingBox to be converted
+/// \return Pose type filled with centroid and orientation from box
+geometry_msgs::msg::Pose GEOMETRY_PUBLIC make_pose(const BoundingBox & box);
+
+/// \brief Fill DetectedObject type with contents from a BoundingBox type
+/// \param box BoundingBox to be converted
+/// \return Filled DetectedObject type
+autoware_auto_msgs::msg::DetectedObject GEOMETRY_PUBLIC make_detected_object(
+  const BoundingBox & box);
 
 }  // namespace details
 }  // namespace bounding_box

src/common/autoware_auto_geometry/src/bounding_box.cpp
@@ -23,6 +23,7 @@
 #include <algorithm>
 #include <limits>
 #include <list>
+#include <utility>
 #include <vector>
 
 namespace autoware
@@ -63,8 +64,55 @@ void finalize_box(const decltype(BoundingBox::corners) & corners, BoundingBox & 
   // centroid
   box.centroid = times_2d(plus_2d(corners[0U], corners[2U]), 0.5F);
 }
+
+
+autoware_auto_msgs::msg::Shape make_shape(const BoundingBox & box)
+{
+  autoware_auto_msgs::msg::Shape ret;
+  for (const auto & corner_pt : box.corners) {
+    ret.polygon.points.emplace_back(corner_pt);
+  }
+  return ret;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+geometry_msgs::msg::Pose make_pose(const BoundingBox & box)
+{
+  geometry_msgs::msg::Pose ret;
+  ret.position.x = static_cast<double>(box.centroid.x);
+  ret.position.y = static_cast<double>(box.centroid.y);
+  ret.position.z = static_cast<double>(box.centroid.z);
+
+  ret.orientation.x = static_cast<double>(box.orientation.x);
+  ret.orientation.y = static_cast<double>(box.orientation.y);
+  ret.orientation.z = static_cast<double>(box.orientation.z);
+  ret.orientation.w = static_cast<double>(box.orientation.w);
+
+  return ret;
+}
+
+autoware_auto_msgs::msg::DetectedObject make_detected_object(const BoundingBox & box)
+{
+  autoware_auto_msgs::msg::DetectedObject ret;
+
+  ret.kinematics.has_pose = true;
+  ret.kinematics.pose.pose = make_pose(box);
+
+  ret.shape = make_shape(box);
+
+  ret.existence_probability = 1.0F;
+
+  autoware_auto_msgs::msg::ObjectClassification label;
+  label.classification = autoware_auto_msgs::msg::ObjectClassification::UNKNOWN;
+  label.probability = 1.0F;
+  ret.classification.emplace_back(std::move(label));
+
+  return ret;
+}
+
 }  // namespace details
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // precompilation
 using autoware::common::types::PointXYZIF;
 template BoundingBox minimum_area_bounding_box<PointXYZIF>(std::list<PointXYZIF> & list);

src/perception/segmentation/euclidean_cluster/src/shape_estimation.cpp
@@ -1,0 +1,204 @@
+// Copyright 2021 Apex.AI, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <euclidean_cluster/shape_estimation.hpp>
+#include <lidar_utils/point_cloud_utils.hpp>
+#include <geometry/bounding_box_2d.hpp>
+
+namespace autoware
+{
+namespace perception
+{
+namespace segmentation
+{
+namespace euclidean_cluster
+{
+
+////////////////////////////////////////////////////////////////////////////////
+ComputeLfitBoundingBoxes::ComputeLfitBoundingBoxes(
+  const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeLfitBoundingBoxes::operator()(
+  autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::make_detected_object(
+          common::geometry::bounding_box::lfit_bounding_box_2d(
+            iter_pair.first, iter_pair.second)));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeLfitBoundingBoxes::operator()(autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::lfit_bounding_box_2d(iter_pair.first, iter_pair.second));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+ComputeLfitBoundingBoxesWithZ::ComputeLfitBoundingBoxesWithZ(
+  const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeLfitBoundingBoxesWithZ::operator()(
+  autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::make_detected_object(
+          common::geometry::bounding_box::lfit_bounding_box_2d(
+            iter_pair.first,
+            iter_pair.second)));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, objects_msg.objects.back().shape);
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeLfitBoundingBoxesWithZ::operator()(autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::lfit_bounding_box_2d(iter_pair.first, iter_pair.second));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, boxes.boxes.back());
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+ComputeEigenBoxes::ComputeEigenBoxes(const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeEigenBoxes::operator()(autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::make_detected_object(
+          common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second)));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeEigenBoxes::operator()(autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second));
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+ComputeEigenBoxesWithZ::ComputeEigenBoxesWithZ(
+  const autoware_auto_msgs::msg::PointClusters & clusters)
+: m_clusters(clusters) {}
+
+void ComputeEigenBoxesWithZ::operator()(
+  autoware_auto_msgs::msg::DetectedObjects & objects_msg)
+{
+  objects_msg.objects.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      objects_msg.objects.push_back(
+        common::geometry::bounding_box::details::make_detected_object(
+          common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second)));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, objects_msg.objects.back().shape);
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+
+void ComputeEigenBoxesWithZ::operator()(
+  autoware_auto_msgs::msg::BoundingBoxArray & boxes)
+{
+  boxes.boxes.clear();
+  for (uint32_t cls_id = 0U; cls_id < m_clusters.cluster_boundary.size(); cls_id++) {
+    try {
+      const auto iter_pair = common::lidar_utils::get_cluster(m_clusters, cls_id);
+      if (iter_pair.first == iter_pair.second) {
+        continue;
+      }
+      boxes.boxes.push_back(
+        common::geometry::bounding_box::eigenbox_2d(iter_pair.first, iter_pair.second));
+      common::geometry::bounding_box::compute_height(
+        iter_pair.first, iter_pair.second, boxes.boxes.back());
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+}  // namespace euclidean_cluster
+}  // namespace segmentation
+}  // namespace perception
+}  // namespace autoware