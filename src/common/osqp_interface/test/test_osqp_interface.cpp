bb620744cf82a1001c01f05d9a0bec14a336dceb
Maxime CLEMENT <maxime.clement@tier4.jp>
2021-06-28 17:37:40 +0900

    [#1057] Add osqp_interface pkg copied from architecture proposal

---
+++ src/common/osqp_interface/test/test_osqp_interface.cpp
@@ -0,0 +1,20 @@
+// Copyright 2021 The Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gtest/gtest.h"
+#include "osqp_interface/osqp_interface.hpp"
+
+TEST(test_osqp_interface, test_hello) {
+  EXPECT_EQ(autoware::osqp_interface::print_hello(), 0);
+}
