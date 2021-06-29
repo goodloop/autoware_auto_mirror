89afba8475bac584c1351bb9beceac9137b40bb4
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-06-29 16:37:34 +0900

    Add functionality in the filter and get_node_parameters in the voxel_grid_outlier_filter  - Needed to change types of float -> double, uint32_t -> int32_t

--- src/perception/filters/outlier_filter_nodes/src/voxel_grid_outlier_filter_node.cpp
+++ src/perception/filters/outlier_filter_nodes/src/voxel_grid_outlier_filter_node.cpp
@@ -17,6 +17,8 @@
 
 #include "outlier_filter_nodes/voxel_grid_outlier_filter_node.hpp"
 
+#include "pcl_conversions/pcl_conversions.h"
+
 
 namespace autoware
 {
@@ -38,22 +40,60 @@ VoxelGridOutlierFilterNode::VoxelGridOutlierFilterNode(const rclcpp::NodeOptions
     declare_parameter("voxel_size_x").get<float>(),
     declare_parameter("voxel_size_y").get<float>(),
     declare_parameter("voxel_size_z").get<float>(),
-    declare_parameter("voxel_point_threshold").get<uint32_t>());
+    declare_parameter("voxel_points_threshold").get<uint32_t>());
 }
 
 void VoxelGridOutlierFilterNode::filter(
-  const sensor_msgs::msg::PointCloud2 &,
-  sensor_msgs::msg::PointCloud2 &)
+  const sensor_msgs::msg::PointCloud2 & input, sensor_msgs::msg::PointCloud2 & output)
 {
+  pcl::PointCloud<pcl::PointXYZ> pcl_input;
+  pcl::PointCloud<pcl::PointXYZ> pcl_output;
+  pcl::fromROSMsg(input, pcl_input);
 
+  // Perform the filtering
+  voxel_grid_outlier_filter_->filter(pcl_input, pcl_output);
+
+  pcl::toROSMsg(pcl_output, output);
 }
 
 rcl_interfaces::msg::SetParametersResult VoxelGridOutlierFilterNode::get_node_parameters(
-  const std::vector<rclcpp::Parameter> &)
+  const std::vector<rclcpp::Parameter> & p)
 {
   rcl_interfaces::msg::SetParametersResult result;
   result.successful = true;
   result.reason = "";
+
+  {
+    using namespace autoware::perception::filters::filter_node_base; //NOLINT
+
+    if (get_param<double>(p, "voxel_size_x", voxel_size_x_)) {
+      result.successful = false;
+      result.reason += "Failed to retrieve voxel_size_x parameter. ";
+      RCLCPP_DEBUG(get_logger(), "Setting new voxel leaf size (x) to: %f.", voxel_size_x_);
+    }
+    if (get_param<double>(p, "voxel_size_y", voxel_size_y_)) {
+      result.successful = false;
+      result.reason += "Failed to retrieve voxel_size_y parameter. ";
+      RCLCPP_DEBUG(get_logger(), "Setting new voxel leaf size (y) to: %f.", voxel_size_y_);
+    }
+    if (get_param<double>(p, "voxel_size_z", voxel_size_z_)) {
+      result.successful = false;
+      result.reason += "Failed to retrieve voxel_size_z parameter. ";
+      RCLCPP_DEBUG(get_logger(), "Setting new voxel leaf size (z) to: %f.", voxel_size_z_);
+    }
+    if (get_param<int64_t>(p, "voxel_points_threshold", voxel_points_threshold_)) {
+      result.successful = false;
+      result.reason += "Failed to retrieve voxel_points_threshold parameter. ";
+      RCLCPP_DEBUG(
+        get_logger(), "Setting new voxel points threshold to: %d.", voxel_points_threshold_);
+    }
+  }
+
+  // Call update method
+  voxel_grid_outlier_filter_->update_parameters(
+    static_cast<float>(voxel_size_x_), static_cast<float>(voxel_size_y_),
+    static_cast<float>(voxel_size_z_),
+    static_cast<uint32_t>(voxel_points_threshold_));
 
   return result;
 }
