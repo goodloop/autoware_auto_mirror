6f4f438ff65451244bba8c8619e6d6e4d21a0427
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-06-29 14:56:51 +0900

    Add skeleton for voxel_grid_outlier_filter_nodes node class

---
+++ src/perception/filters/outlier_filter_nodes/src/voxel_grid_outlier_filter_node.cpp
@@ -0,0 +1,63 @@
+// Copyright 2021 The Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include <vector>
+
+#include "outlier_filter_nodes/voxel_grid_outlier_filter_node.hpp"
+
+
+namespace autoware
+{
+namespace perception
+{
+namespace filters
+{
+namespace outlier_filter_nodes
+{
+
+VoxelGridOutlierFilterNode::VoxelGridOutlierFilterNode(const rclcpp::NodeOptions & options)
+: FilterNodeBase("voxel_grid_outlier_filter_node", options)
+{
+
+}
+
+void VoxelGridOutlierFilterNode::filter(
+  const sensor_msgs::msg::PointCloud2 &,
+  sensor_msgs::msg::PointCloud2 &)
+{
+
+}
+
+rcl_interfaces::msg::SetParametersResult VoxelGridOutlierFilterNode::get_node_parameters(
+  const std::vector<rclcpp::Parameter> &)
+{
+  rcl_interfaces::msg::SetParametersResult result;
+  result.successful = true;
+  result.reason = "";
+
+  return result;
+}
+
+}  // namespace outlier_filter_nodes
+}  // namespace filters
+}  // namespace perception
+}  // namespace autoware
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// This acts as an entry point, allowing the component to be
+// discoverable when its library is being loaded into a running process
+RCLCPP_COMPONENTS_REGISTER_NODE(
+  autoware::perception::filters::outlier_filter_nodes::VoxelGridOutlierFilterNode)
