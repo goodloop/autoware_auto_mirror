0ebd4626c77b7841ac98ccda7fbd53d4f17b27b9
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-06-30 15:38:36 +0900

    Add documentation to radius_search_2d_filter_node files

--- src/perception/filters/outlier_filter_nodes/include/outlier_filter_nodes/radius_search_2d_filter_node.hpp
+++ src/perception/filters/outlier_filter_nodes/include/outlier_filter_nodes/radius_search_2d_filter_node.hpp
@@ -1,4 +1,4 @@
-// Copyright 2021 The Autoware Foundation
+// Copyright 2021 Tier IV, Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// \copyright Copyright 2021 The Autoware Foundation
+/// \copyright Copyright 2021 Tier IV, Inc
 /// \file
-/// \brief This file defines the outlier_filter_nodes_node class.
+/// \brief This file defines the RadiusSearch2DFilterNode class.
 
 #ifndef OUTLIER_FILTER_NODES__RADIUS_SEARCH_2D_FILTER_NODE_HPP_
 #define OUTLIER_FILTER_NODES__RADIUS_SEARCH_2D_FILTER_NODE_HPP_
@@ -43,28 +43,53 @@ using FilterNodeBase = autoware::perception::filters::filter_node_base::FilterNo
 using RadiusSearch2DFilter =
   autoware::perception::filters::outlier_filter::radius_search_2d_filter::
   RadiusSearch2DFilter;
-/// \class OutlierFilterNodesNode
-/// \brief ROS 2 Node for hello world.
+
+/// \class RadiusSearch2DFilterNode
+/// \brief Node inheriting from FilterNodeBase and is the ROS2 node interface for the
+//   RadiusSearch2DFilter library
 class OUTLIER_FILTER_NODES_PUBLIC RadiusSearch2DFilterNode : public FilterNodeBase
 {
 public:
-  /// \brief default constructor, starts driver
-  /// \throw runtime error if failed to start threads or configure driver
+  /** \brief The default constructor for the RadiusSearch2DFilterNode class
+   * \param options An rclcpp::NodeOptions object to pass on to the FilterNodeBase class
+   */
   explicit RadiusSearch2DFilterNode(const rclcpp::NodeOptions & options);
 
 protected:
+  /** \brief Implementation of the FilterNodeBase class abstract filter method
+   *
+   * Converts the point cloud into the PCL::PointCloud<pcl::PointXYZ> type to be processed by the
+   * RadiusSearch2DFilter library. The method then returns the filter point cloud via the output
+   * argument.
+   *
+   * \param input The input point cloud dataset.
+   * \param output The resultant filtered PointCloud2
+   */
   OUTLIER_FILTER_NODES_PUBLIC virtual void filter(
     const sensor_msgs::msg::PointCloud2 & input,
     sensor_msgs::msg::PointCloud2 & output);
 
+  /** \brief Implementation of the FilterNodeBase class abstract get_node_parameters method
+   *
+   * This method is called when a parameter is set. From the resulting parameters it will get all of
+   * the associated parameters and update the filter library class with the new values. The result
+   * of retrieving the parameter is returned.
+   *
+   * \param p Vector of rclcpp::Parameters belonging to the node
+   * \return rcl_interfaces::msg::SetParametersResult Result of retrieving the parameter
+   */
   OUTLIER_FILTER_NODES_PUBLIC virtual rcl_interfaces::msg::SetParametersResult get_node_parameters(
     const std::vector<rclcpp::Parameter> & p);
 
 private:
+  /* Class object containing the RadiusSearch2DFilter library functionality */
   std::shared_ptr<RadiusSearch2DFilter> radius_search_2d_filter_;
 
+  /* Variable containing the value of the search radius (passed into radius_search_2d_filter_) */
   double search_radius_;
 
+  /* Variable containing the value of the minimum neighbors for points (passed into
+     radius_search_2d_filter_) */
   int64_t min_neighbors_;
 };
 }  // namespace outlier_filter_nodes
