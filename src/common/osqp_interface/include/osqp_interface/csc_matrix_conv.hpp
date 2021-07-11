4ba68e5338aa398e6af57bc61e05943636e806ba
Maxime CLEMENT <maxime.clement@tier4.jp>
2021-07-12 05:17:20 +0900

    [#1057] Fix #include for c_int type

--- src/common/osqp_interface/include/osqp_interface/csc_matrix_conv.hpp
+++ src/common/osqp_interface/include/osqp_interface/csc_matrix_conv.hpp
@@ -15,10 +15,10 @@
 #ifndef OSQP_INTERFACE__CSC_MATRIX_CONV_HPP_
 #define OSQP_INTERFACE__CSC_MATRIX_CONV_HPP_
 
-#include <types.h>  // for 'c_int' type ('long' or 'long long')
 #include <vector>
 
 #include "eigen3/Eigen/Core"
+#include "osqp/glob_opts.h"  // for 'c_int' type ('long' or 'long long')
 #include "osqp_interface/visibility_control.hpp"
 
 namespace autoware
