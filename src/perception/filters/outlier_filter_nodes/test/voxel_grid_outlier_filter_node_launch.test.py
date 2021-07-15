3aef6cca07befbd9840cc721850cfb3bb4034f48
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-07-15 18:39:00 +0900

    Add outlier_filter_node node launch tests  - Fix export of parameters  - Add patch from #1205 by calling set_param_callback method in nodes

--- src/perception/filters/outlier_filter_nodes/test/voxel_grid_outlier_filter_node_launch.test.py
+++ src/perception/filters/outlier_filter_nodes/test/voxel_grid_outlier_filter_node_launch.test.py
@@ -27,20 +27,20 @@ import pytest
 @pytest.mark.launch_test
 def generate_test_description():
 
-    radius_search_2d_filter_node = Node(
+    voxel_grid_outlier_filter_node = Node(
         package='outlier_filter_nodes',
-        executable='radius_search_2d_filter_node_exe',
+        executable='voxel_grid_outlier_filter_node_exe',
         namespace='test',
         parameters=[os.path.join(
             get_package_share_directory('outlier_filter_nodes'),
-            'param/radius_search_2d_filter_node_test.param.yaml'
+            'param/voxel_grid_outlier_filter_node_test.param.yaml'
         )]
     )
 
-    context = {'radius_search_2d_filter_node': radius_search_2d_filter_node}
+    context = {'voxel_grid_outlier_filter_node': voxel_grid_outlier_filter_node}
 
     return LaunchDescription([
-        radius_search_2d_filter_node,
+        voxel_grid_outlier_filter_node,
         # Start tests right away - no need to wait for anything
         launch_testing.actions.ReadyToTest()]
     ), context
@@ -49,9 +49,10 @@ def generate_test_description():
 @launch_testing.post_shutdown_test()
 class TestProcessOutput(unittest.TestCase):
 
-    def test_exit_code(self, proc_output, proc_info, radius_search_2d_filter_node):
+    def test_exit_code(self, proc_output, proc_info, voxel_grid_outlier_filter_node):
+        # Check that process exits with code -15 code: termination request, sent to the program
         launch_testing.asserts.assertExitCodes(
             proc_info,
             [-15],
-            process=radius_search_2d_filter_node
+            process=voxel_grid_outlier_filter_node
         )
