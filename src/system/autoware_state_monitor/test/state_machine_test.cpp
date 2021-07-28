8940b7fe09d100b4f0c49ae787362da42328159f
Michal Drwiega <michal.drwiega@robotec.ai>
2021-07-28 13:50:04 +0200

    Add unit test template

---
+++ src/system/autoware_state_monitor/test/state_machine_test.cpp
@@ -0,0 +1,41 @@
+// Copyright 2021 Robotec.ai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "autoware_state_monitor/state_machine.hpp"
+
+
+#include <memory>
+#include <gtest/gtest.h>
+
+using namespace autoware::state_monitor;
+
+class StateMachineTest : public ::testing::Test
+{
+public:
+  StateMachineTest() {
+    StateParam params;
+    params.th_arrived_distance_m = 1.0;
+    params.th_stopped_time_sec = 2.0;
+    params.th_stopped_velocity_mps = 0.1;
+
+    state_machine.reset(new StateMachine(params));
+  }
+
+protected:
+  std::shared_ptr<StateMachine> state_machine;
+};
+
+TEST_F(StateMachineTest, create_destroy) {
+  state_machine.reset();
+}
