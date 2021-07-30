4ed717d1459efd9a85a5afabcf8c3dd6207b64c9
Jacob Johnson <jjj025@eng.ucsd.edu>
2021-07-30 11:19:05 -0700

    set up all virtual functions

--- src/drivers/vesc_interface/include/vesc_interface/vesc_interface.hpp
+++ src/drivers/vesc_interface/include/vesc_interface/vesc_interface.hpp
@@ -23,7 +23,30 @@
 
 #include <vesc_interface/visibility_control.hpp>
 
+#include <common/types.hpp>
+#include <vehicle_interface/platform_interface.hpp>
+
+#include <autoware_auto_msgs/msg/high_level_control_command.hpp>
+#include <autoware_auto_msgs/msg/raw_control_command.hpp>
+#include <autoware_auto_msgs/msg/vehicle_control_command.hpp>
+#include <autoware_auto_msgs/msg/vehicle_state_command.hpp>
+#include <autoware_auto_msgs/msg/vehicle_odometry.hpp>
+#include <autoware_auto_msgs/msg/vehicle_state_report.hpp>
+
+#include <autoware_auto_msgs/msg/vehicle_kinematic_state.hpp>
+
+#include <autoware_auto_msgs/srv/autonomy_mode_change.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <chrono>
 #include <cstdint>
+
+using autoware_auto_msgs::msg::RawControlCommand;
+using autoware_auto_msgs::msg::VehicleControlCommand;
+using autoware_auto_msgs::msg::VehicleStateCommand;
+using autoware_auto_msgs::msg::VehicleStateReport;
+using autoware_auto_msgs::msg::VehicleOdometry;
 
 namespace autoware
 {
@@ -31,31 +54,53 @@ namespace autoware
 namespace vesc_interface
 // Inherit from vehicle_interface::platform_interface
 {
-// update() - nothing, send True
+/// Platform interface implementation for VESC. Bridges data to and from the VESC 
+/// to convert speed and wheel angle position to motor speed and servo position.
+/// \brief Class for interfacing with VESC
+class VESC_INTERFACE_PUBLIC VESCInterface 
+    : public ::autoware::drivers::vehicle_interface::PlatformInterface
+{
+public:
+    /// \brief Default Constructor.
+    /// \todo 
+    VESCInterface(
+        rclcpp::Node &node
+    );
+    /// \brief Default Destructor
+    ~VESCInterface() noexcept override = default;
+
+    /// \brief Sends True message
+    bool8_t update(std::chrono::nanoseconds timeout);
 
 // send_state_command() - Most for gears, trasmit the gear to VESC driver
 // Gear equivalent of reverse/ forward 
+    bool8_t send_state_command(const VehicleControlCommand &msg);
 
 // send_control_command() - desired speed and desired tire angle
 // Convert those to motorRPM, servo positions
 // RawControlCommand - Log NotSupported!!
 // IF mannual mode - send zeros.
-// 
+    bool8_t send_control_command(const VehicleControlCommand &msg);
 
 // handle_mode_change_request()
 // Switch between autonomous, and manual mode
 // maintain interal state, and only send commands when autonomous mode is active
 // IF mannual mode - send zeros. 
 
+    /// \brief Send raw control commands, currently not implemented, hence logs error.
+    bool8_t send_control_command(const RawControlCommand &msg);
+
+protected:
+
 // state_report() -> Set the gear (forward/backward)
+    VehicleStateReport & state_report();
 
 // odometry() -> velocity_mps meters/s
 //               front_wheel_angle_rad (radians, positive-to the left)
 // https://gitlab.com/autowarefoundation/autoware.auto/autoware_auto_msgs/-/blob/master/autoware_auto_msgs/msg/VehicleOdometry.idl
+    VehicleOdometry & get_odometry();
 
-/// \brief TODO(jjj025): Document your functions
-int32_t VESC_INTERFACE_PUBLIC print_hello();
-
+private:
 
 
 }  // namespace vesc_interface
