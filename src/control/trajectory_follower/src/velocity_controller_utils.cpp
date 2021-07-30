8e8917f631729d85aab41998f72c6d85d0897ec8
Maxime CLEMENT <maxime.clement@tier4.jp>
2021-07-30 17:56:17 +0900

    [#1058] Fix cpplint (include order, header guard format)

--- src/control/trajectory_follower/src/velocity_controller_utils.cpp
+++ src/control/trajectory_follower/src/velocity_controller_utils.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <algorithm>
 #include <experimental/optional>
+#include <algorithm>
 #include <limits>
 
 #include "trajectory_follower/velocity_controller_utils.hpp"
