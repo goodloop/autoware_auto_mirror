e6bcf00c9d0fd05baad8ec5f7d9d97910fb5d3de
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-08-03 15:28:06 +0200

    Rebase on top of master

--- src/drivers/lgsvl_interface/include/lgsvl_interface/lgsvl_interface.hpp
+++ src/drivers/lgsvl_interface/include/lgsvl_interface/lgsvl_interface.hpp
@@ -21,9 +21,7 @@
 #include <lgsvl_interface/visibility_control.hpp>
 
 #include <autoware_auto_msgs/msg/headlights_command.hpp>
-#include <autoware_auto_msgs/msg/headlights_report.hpp>
 #include <autoware_auto_msgs/msg/horn_command.hpp>
-#include <autoware_auto_msgs/msg/horn_report.hpp>
 #include <autoware_auto_msgs/msg/raw_control_command.hpp>
 #include <autoware_auto_msgs/msg/vehicle_kinematic_state.hpp>
 #include <autoware_auto_msgs/msg/vehicle_state_command.hpp>
@@ -104,8 +102,6 @@ public:
     Table1D && throttle_table,
     Table1D && brake_table,
     Table1D && steer_table,
-    rclcpp::Publisher<autoware_auto_msgs::msg::HeadlightsReport>::SharedPtr headlights_report_pub,
-    rclcpp::Publisher<autoware_auto_msgs::msg::HornReport>::SharedPtr horn_report_pub,
     bool publish_tf = NO_PUBLISH,
     bool publish_pose = PUBLISH);
 
