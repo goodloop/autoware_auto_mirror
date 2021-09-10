da51beaa526c457a89f1b27bb4d1819dfc561163
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-09-10 10:47:58 +0200

    Replace add_point_to_cloud

--- src/perception/filters/point_cloud_filter_transform_nodes/test/test_point_cloud_filter_transform.cpp
+++ src/perception/filters/point_cloud_filter_transform_nodes/test/test_point_cloud_filter_transform.cpp
@@ -78,18 +78,17 @@ sensor_msgs::msg::PointCloud2 make_pc(
   builtin_interfaces::msg::Time stamp)
 {
   sensor_msgs::msg::PointCloud2 msg;
-  using autoware::common::types::PointXYZI;
-  point_cloud_msg_wrapper::PointCloud2Modifier<PointXYZI> modifier{msg, "base_link"};
-  modifier.resize(seeds.size());
+  using autoware::common::types::PointXYZIF;
+  point_cloud_msg_wrapper::PointCloud2Modifier<PointXYZIF> modifier{msg, "base_link"};
+  modifier.reserve(seeds.size());
 
-  uint32_t pidx = 0;
   for (auto seed : seeds) {
-    autoware::common::types::PointXYZIF pt;
+    PointXYZIF pt;
     pt.x = seed;
     pt.y = seed;
     pt.z = seed;
     pt.intensity = seed;
-    autoware::common::lidar_utils::add_point_to_cloud(msg, pt, pidx);
+    modifier.push_back(pt);
   }
 
   msg.header.stamp = stamp;
@@ -302,25 +301,28 @@ TEST_F(PointCloudFilterTransformIntegration, Filter270Radius10) {
 
   std::vector<std::vector<PointXYZIF>> expected_filter_output_points(1);
   PointCloud2 raw_msg;
-  using autoware::common::types::PointXYZI;
-  point_cloud_msg_wrapper::PointCloud2Modifier<PointXYZI> modifier{raw_msg, "lidar_front"};
-  modifier.resize(5);
+  using autoware::common::types::PointXYZIF;
+  point_cloud_msg_wrapper::PointCloud2Modifier<PointXYZIF> modifier{raw_msg, "lidar_front"};
+  modifier.reserve(5);
 
-  uint32_t pidx = 0;
   PointXYZIF pt;
   pt.x = 1.; pt.y = 2.; pt.z = 3.;
-  (void)autoware::common::lidar_utils::add_point_to_cloud(raw_msg, pt, pidx);
+  modifier.push_back(pt);
   expected_filter_output_points[0].push_back(pt);
+
   pt.x = -1.; pt.y = 2.; pt.z = 3.;
-  (void)autoware::common::lidar_utils::add_point_to_cloud(raw_msg, pt, pidx);
+  modifier.push_back(pt);
   expected_filter_output_points[0].push_back(pt);
+
   pt.x = -1.; pt.y = -2.; pt.z = 3.;
-  (void)autoware::common::lidar_utils::add_point_to_cloud(raw_msg, pt, pidx);
+  modifier.push_back(pt);
   expected_filter_output_points[0].push_back(pt);
+
   pt.x = 1.; pt.y = -2.; pt.z = 3.;  // not within angle_filter
-  (void)autoware::common::lidar_utils::add_point_to_cloud(raw_msg, pt, pidx);
+  modifier.push_back(pt);
+
   pt.x = -1.; pt.y = -11.; pt.z = 3.;  // not within distance filter
-  (void)autoware::common::lidar_utils::add_point_to_cloud(raw_msg, pt, pidx);
+  modifier.push_back(pt);
 
   raw_msg.row_step = raw_msg.width * raw_msg.point_step;
 
