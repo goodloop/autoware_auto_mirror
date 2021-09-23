62a3662a51df12ba6b0e479d05206db8f9acb9fb
danielm1405 <daniel.marczak@robotec.ai>
2021-09-23 15:30:02 +0200

    fix licenses

--- src/launch/scenario_simulator_launch/launch/autoware_auto.launch.py
+++ src/launch/scenario_simulator_launch/launch/autoware_auto.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021, The Autoware Foundation
+# Copyright 2021 The Autoware Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Developed by Robotec.ai.
+# Co-developed by Tier IV, Inc. and Robotec.AI sp. z o.o.
 
 """Launch required subsystems for running scenario simulator."""
 
