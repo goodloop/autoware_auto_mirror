9109fc79c52167831e5fb9b85bc8cd57c7f929a4
bnk <bnk@leodrive.ai>
2021-12-20 17:31:39 +0300

    update benchmark for new intersection function

--- src/common/autoware_auto_geometry/test/bench/bench_intersection.cpp
+++ src/common/autoware_auto_geometry/test/bench/bench_intersection.cpp
@@ -3,21 +3,26 @@
 #include <geometry/intersection.hpp>
 #include <benchmark/benchmark.h>
 
+
 struct TestPoint
 {
     autoware::common::types::float32_t x;
     autoware::common::types::float32_t y;
 };
 
+using point_type = autoware::common::geometry::bg::model::point<double, 2, autoware::common::geometry::bg::cs::cartesian>;
+using polygon_type = autoware::common::geometry::bg::model::ring<point_type>;
+
 static void bench_convex_polygon_intersection_2d(benchmark::State& state) {
 
-    std::list<TestPoint> polygon1{{ 6, 1},{ 6, 8},{ 2, 8},{-2, 4},{ 1, 1},{ 6, 1}};
-    std::list<TestPoint> polygon2{{1, 2},{2, 6},{4, 2},{1, 2}};
+    polygon_type polygon1{{ 6, 1},{ 6, 8},{ 2, 8},{-2, 4},{ 1, 1},{ 6, 1}};
+    polygon_type polygon2{{1, 2},{2, 6},{4, 2},{1, 2}};
 
     for (auto _ : state)
         autoware::common::geometry::convex_polygon_intersection2d(polygon1, polygon2);
 
 }
+
 
 // Register the function as a benchmark
 
