8195d341ef21d7c53c70af7d9a4625b5b3dd50bc
kielczykowski-rai <michal.kielczykowski@robotec.ai>
2021-12-27 14:34:15 +0100

    Split parameter structures, change namespacing and adjust node config. TODO change visibility names and package name

--- src/planning/freespace_planner/src/freespace_planner/freespace_planner_node.cpp
+++ src/planning/freespace_planner/src/freespace_planner/freespace_planner_node.cpp
@@ -46,22 +46,22 @@ geometry_msgs::msg::Pose transformPose(
   return transformed_pose.pose;
 }
 
-astar_search::AstarWaypoints adjustWaypointsSize(
-  const astar_search::AstarWaypoints & astar_waypoints)
+parking::PlannerWaypoints adjustWaypointsSize(
+  const parking::PlannerWaypoints & planner_waypoints)
 {
   auto max_length = Trajectory::CAPACITY;
-  auto input_length = astar_waypoints.waypoints.size();
+  auto input_length = planner_waypoints.waypoints.size();
 
   if (input_length > max_length) {
-    astar_search::AstarWaypoints resized_vector;
-    resized_vector.header = astar_waypoints.header;
+    parking::PlannerWaypoints resized_vector;
+    resized_vector.header = planner_waypoints.header;
 
     // input_length substraction to handle handle max_length multiplicity
     auto elements_to_skip_per_step = static_cast<int64_t>(std::floor(input_length / max_length));
     // must be +1 to actually skip an element
     auto stride = elements_to_skip_per_step + 1;
 
-    auto waypoints_iter = astar_waypoints.waypoints.begin();
+    auto waypoints_iter = planner_waypoints.waypoints.begin();
     auto points_to_skip = static_cast<int64_t>(input_length - max_length);
     int64_t skipped_points_count = 0;
     // substract by elements_to_skip_per_step to prevent from skipping too many points
@@ -73,22 +73,22 @@ astar_search::AstarWaypoints adjustWaypointsSize(
 
     // copy rest of waypoints
     resized_vector.waypoints.insert(
-      resized_vector.waypoints.end(), waypoints_iter, astar_waypoints.waypoints.end());
+      resized_vector.waypoints.end(), waypoints_iter, planner_waypoints.waypoints.end());
     return resized_vector;
   }
 
-  return astar_waypoints;
+  return planner_waypoints;
 }
 
 autoware_auto_planning_msgs::msg::Trajectory createTrajectory(
   const geometry_msgs::msg::PoseStamped & current_pose,
-  const astar_search::AstarWaypoints & astar_waypoints,
+  const parking::PlannerWaypoints & planner_waypoints,
   const float & velocity)
 {
   Trajectory trajectory;
-  trajectory.header = astar_waypoints.header;
+  trajectory.header = planner_waypoints.header;
 
-  for (const auto & awp : astar_waypoints.waypoints) {
+  for (const auto & awp : planner_waypoints.waypoints) {
     autoware_auto_planning_msgs::msg::TrajectoryPoint point;
 
     point.pose.position.x = awp.pose.pose.position.x;
@@ -123,50 +123,49 @@ FreespacePlannerNode::FreespacePlannerNode(const rclcpp::NodeOptions & node_opti
   // NodeParam
   {
     node_param_.waypoints_velocity = declare_parameter("waypoints_velocity", 5.0);
+    node_param_.planning_algorithm = declare_parameter("planning_algorithm", "astar");
   }
 
-  // AstarParam
+  // PlannerCommonParam
   {
+  auto & param = planner_common_param_;
     // base configs
-    astar_param_.use_back = declare_parameter("use_back", true);
-    astar_param_.only_behind_solutions = declare_parameter("only_behind_solutions", false);
-    astar_param_.time_limit = declare_parameter("time_limit", 5000.0);
+    param.time_limit = declare_parameter("time_limit", 5000.0);
 
     auto vehicle_dimension_margin = declare_parameter("vehicle_dimension_margin", 0.0);
     auto vehicle_constants = declare_and_get_vehicle_constants(*this);
-    astar_param_.robot_shape.length = vehicle_constants.vehicle_length + vehicle_dimension_margin;
-    astar_param_.robot_shape.width = vehicle_constants.vehicle_width + vehicle_dimension_margin;
-    astar_param_.robot_shape.cg2back =
+    param.vehicle_shape.length = vehicle_constants.vehicle_length + vehicle_dimension_margin;
+    param.vehicle_shape.width = vehicle_constants.vehicle_width + vehicle_dimension_margin;
+    param.vehicle_shape.cg2back =
       vehicle_constants.cg_to_rear + vehicle_constants.overhang_rear +
       (vehicle_dimension_margin / 2.0);
-    astar_param_.minimum_turning_radius = vehicle_constants.minimum_turning_radius;
+    param.minimum_turning_radius = vehicle_constants.minimum_turning_radius;
 
-    astar_param_.maximum_turning_radius = declare_parameter("maximum_turning_radius", 6.0);
-    astar_param_.maximum_turning_radius = std::max(
-      astar_param_.maximum_turning_radius,
-      astar_param_.minimum_turning_radius);
+    param.maximum_turning_radius = declare_parameter("maximum_turning_radius", 6.0);
+    param.maximum_turning_radius = std::max(
+      param.maximum_turning_radius,
+      param.minimum_turning_radius);
     auto tr_size = declare_parameter("turning_radius_size", 11);
     throw_if_negative(tr_size, "turning_radius_size");
-    astar_param_.turning_radius_size = static_cast<size_t>(tr_size);
+    param.turning_radius_size = static_cast<size_t>(tr_size);
 
     // search configs
     auto th_size = declare_parameter("theta_size", 48);
     throw_if_negative(th_size, "theta_size");
-    astar_param_.theta_size = static_cast<size_t>(th_size);
-    astar_param_.reverse_weight = declare_parameter("reverse_weight", 2.00);
-    astar_param_.goal_lateral_tolerance = declare_parameter("goal_lateral_tolerance", 0.25);
-    astar_param_.goal_longitudinal_tolerance =
+    param.theta_size = static_cast<size_t>(th_size);
+    param.reverse_weight = declare_parameter("reverse_weight", 2.00);
+    param.goal_lateral_tolerance = declare_parameter("goal_lateral_tolerance", 0.25);
+    param.goal_longitudinal_tolerance =
       declare_parameter("goal_longitudinal_tolerance", 1.0);
-    astar_param_.goal_angular_tolerance = declare_parameter("goal_angular_tolerance", 0.05236);
+    param.goal_angular_tolerance = declare_parameter("goal_angular_tolerance", 0.05236);
 
     // costmap configs
-    astar_param_.obstacle_threshold = declare_parameter("obstacle_threshold", 100);
-    astar_param_.distance_heuristic_weight = declare_parameter("distance_heuristic_weight", 1.0);
+    param.obstacle_threshold = declare_parameter("obstacle_threshold", 100);
   }
 
-  // Hybrid A* implementation
+  // Initialize searching algorithm
   {
-    algo_ = std::make_unique<astar_search::AstarSearch>(astar_param_);
+    initializePlanningAlgorithm();
   }
 
   // Publisher
@@ -217,6 +216,15 @@ FreespacePlannerNode::FreespacePlannerNode(const rclcpp::NodeOptions & node_opti
     tf_listener_ = std::make_shared<tf2_ros::TransformListener>(
       *tf_buffer_, std::shared_ptr<rclcpp::Node>(this, [](auto) {}), false);
   }
+}
+
+parking::AstarParam FreespacePlannerNode::getAstarParam()
+{
+  parking::AstarParam param;
+  param.use_back = declare_parameter("astar.use_back", true);
+  param.only_behind_solutions = declare_parameter("astar.only_behind_solutions", false);
+  param.distance_heuristic_weight = declare_parameter("astar.distance_heuristic_weight", 1.0);
+  return param;
 }
 
 rclcpp_action::GoalResponse FreespacePlannerNode::handleGoal(
@@ -347,25 +355,25 @@ bool FreespacePlannerNode::planTrajectory()
 
   RCLCPP_INFO(get_logger(), "Astar planning took %f [s]", (end - start).seconds());
 
-  if (astar_search::isSuccess(search_status)) {
+  if (parking::isSuccess(search_status)) {
     RCLCPP_INFO(get_logger(), "Plan found.");
     auto waypoints = adjustWaypointsSize(algo_->getWaypoints());
     trajectory_ =
       createTrajectory(start_pose_, waypoints, static_cast<float>(node_param_.waypoints_velocity));
   } else {
     switch (search_status) {
-      case astar_search::SearchStatus::FAILURE_COLLISION_AT_START:
+      case parking::SearchStatus::FAILURE_COLLISION_AT_START:
         RCLCPP_ERROR(
           get_logger(), "Cannot find plan because collision was detected in start position.");
         break;
-      case astar_search::SearchStatus::FAILURE_COLLISION_AT_GOAL:
+      case parking::SearchStatus::FAILURE_COLLISION_AT_GOAL:
         RCLCPP_ERROR(
           get_logger(), "Cannot find plan because collision was detected in goal position.");
         break;
-      case astar_search::SearchStatus::FAILURE_TIMEOUT_EXCEEDED:
+      case parking::SearchStatus::FAILURE_TIMEOUT_EXCEEDED:
         RCLCPP_ERROR(get_logger(), "Cannot find plan because timeout exceeded.");
         break;
-      case astar_search::SearchStatus::FAILURE_NO_PATH_FOUND:
+      case parking::SearchStatus::FAILURE_NO_PATH_FOUND:
         RCLCPP_ERROR(get_logger(), "Cannot find plan.");
         break;
       default:
@@ -374,7 +382,7 @@ bool FreespacePlannerNode::planTrajectory()
     }
   }
 
-  return astar_search::isSuccess(search_status);
+  return parking::isSuccess(search_status);
 }
 
 bool FreespacePlannerNode::isPlanning() const
@@ -424,6 +432,17 @@ void FreespacePlannerNode::visualizeTrajectory()
 
   // publish trajectory as-is
   trajectory_debug_pub_->publish(trajectory_);
+}
+
+void FreespacePlannerNode::initializePlanningAlgorithm()
+{
+  if (node_param_.planning_algorithm == "astar") {
+    auto algorithm_param = getAstarParam();
+    algo_.reset(new parking::AstarSearch(planner_common_param_, algorithm_param));
+  } else {
+    throw std::runtime_error(
+      "No such algorithm named " + node_param_.planning_algorithm + " exists.");
+  }
 }
 
 }  // namespace freespace_planner
