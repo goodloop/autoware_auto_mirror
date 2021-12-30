296dcc9ab226a850180b68d5c261898459f3ac73
kielczykowski-rai <michal.kielczykowski@robotec.ai>
2021-12-30 11:04:33 +0100

    Refactored ReedsShepp algorithm

--- src/planning/astar_search/include/astar_search/reeds_shepp_impl.hpp
+++ src/planning/astar_search/include/astar_search/reeds_shepp_impl.hpp
@@ -80,11 +80,11 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
-#ifndef ASTAR_SEARCH__REEDS_SHEPP_HPP_
-#define ASTAR_SEARCH__REEDS_SHEPP_HPP_
+#ifndef ASTAR_SEARCH__REEDS_SHEPP_IMPL_HPP_
+#define ASTAR_SEARCH__REEDS_SHEPP_IMPL_HPP_
 
-#include <astar_search/reeds_shepp_impl.hpp>
-
+#include <limits>
+#include <array>
 
 namespace autoware
 {
@@ -92,33 +92,85 @@ namespace planning
 {
 namespace parking
 {
+// structs
+/** \brief The Reeds-Shepp path segment types */
+enum ReedsSheppPathSegmentType {NO_OPERATION, COUNTERCLOCKWISE, STRAIGHT, CLOCKWISE};
 
-struct StateXYT
+/** \brief Reeds-Shepp path configurations */
+extern const ReedsSheppPathSegmentType RP_PATH_TYPE[18][5];
+
+/** \brief Complete description of a ReedsShepp path */
+class ReedsSheppPath
+{
+public:
+  ReedsSheppPath(
+    const ReedsSheppPathSegmentType * type = RP_PATH_TYPE[0],
+    double t = std::numeric_limits<double>::max(),
+    double u = 0.0,
+    double v = 0.0,
+    double w = 0.0,
+    double x = 0.0);
+
+  double length() const {return totalLength_;}
+
+  /** Path segment types */
+  const ReedsSheppPathSegmentType * type_;
+  /** Path segment lengths */
+  std::array<double, 5> length_;
+  /** Total length */
+  double totalLength_;
+};
+
+struct ReedsSheppNode
 {
   double x;
   double y;
-  double yaw;
+  double phi;
+
+  ReedsSheppNode timeflipped() const
+  {
+    ReedsSheppNode node = *this;
+    node.x = -1.0 * node.x;
+    node.phi = -1.0 * node.phi;
+    return node;
+  }
+
+  ReedsSheppNode reflected() const
+  {
+    ReedsSheppNode node = *this;
+    node.y = -1.0 * node.y;
+    node.phi = -1.0 * node.phi;
+    return node;
+  }
 };
 
-class ReedsShepp
+// struct ReedsSheppPathParameter
+// {
+//   double t = std::numeric_limits<double>::max();
+//   double u = 0.0;
+//   double v = 0.0;
+//   double w = 0.0;
+//   double x = 0.0;
+
+//   ReedsSheppPathParameter timeflipped() const
+//   {
+//     ReedsSheppPathParameter parameters = *this;
+//   }
+
+//   ReedsSheppPathParameter reflected() const
+//   {
+
+//   }
+// }
+
+namespace reeds_shepp
 {
-public:
-  explicit ReedsShepp(double turningRadius)
-  : turning_radius_(turningRadius) {}
-
-  double distance(const StateXYT & s0, const StateXYT & s1);
-
-  /** \brief Return the shortest Reeds-Shepp path from SE(2) state state1 to SE(2) state state2 */
-  ReedsSheppPath reedsShepp(const StateXYT & s0, const StateXYT & s1);
-
-protected:
-  ReedsSheppPath reedsShepp(ReedsSheppNode node);
-  void interpolate(const StateXYT & s0, ReedsSheppPath & path, double seg, StateXYT & s_out);
-
-  /** \brief Turning radius */
-  double turning_radius_;
-};
-
+void CSC(ReedsSheppNode node, ReedsSheppPath & path);
+void CCC(ReedsSheppNode node, ReedsSheppPath & path);
+void CCCC(ReedsSheppNode node, ReedsSheppPath & path);
+void CCSC(ReedsSheppNode node, ReedsSheppPath & path);
+void CCSCC(ReedsSheppNode node, ReedsSheppPath & path);
+}  // namespace reeds_shepp
 }  // namespace parking
 }  // namespace planning
 }  // namespace autoware
