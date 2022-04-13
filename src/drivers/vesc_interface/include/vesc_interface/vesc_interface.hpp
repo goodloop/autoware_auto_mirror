ub-cavas/AutowareAuto

[vesc_interface] add rpm change limits to vesc_interface
Signed-off-by: mitsudome-r <ryohsuke.mitsudome@tier4.jp>

src/drivers/vesc_interface/include/vesc_interface/vesc_interface.hpp
@@ -73,6 +73,8 @@ public:
     rclcpp::Node & node,
     double speed_to_erpm_gain,
     double speed_to_erpm_offset,
+    double max_erpm_positive_delta,
+    double max_erpm_negative_delta,
     double steering_to_servo_gain,
     double steering_to_servo_offset
   );
@@ -114,6 +116,7 @@ private:
   rclcpp::Logger m_logger;
   // conversion and gain offsets
   double speed_to_erpm_gain_, speed_to_erpm_offset_;
+  double max_erpm_positive_delta_, max_erpm_negative_delta_;
   double steering_to_servo_gain_, steering_to_servo_offset_;
 
   // Direction:

goodloop/autoware_auto_mirror

src/drivers/vesc_interface/include/vesc_interface/vesc_interface.hpp
@@ -80,6 +80,8 @@ public:
     rclcpp::Node & node,
     float64_t speed_to_erpm_gain,
     float64_t speed_to_erpm_offset,
+    float64_t max_erpm_positive_delta,
+    float64_t max_erpm_negative_delta,
     float64_t steering_to_servo_gain,
     float64_t steering_to_servo_offset
   );
@@ -125,6 +127,7 @@ private:
   rclcpp::Logger m_logger;
   // conversion and gain offsets
   float64_t speed_to_erpm_gain_, speed_to_erpm_offset_;
+  float64_t max_erpm_positive_delta_, max_erpm_negative_delta_;
   float64_t steering_to_servo_gain_, steering_to_servo_offset_;
   Float64::SharedPtr last_servo_cmd;
 